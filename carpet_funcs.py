e9144bd97bfdcc1437965549bbf0dddfd275b7fb
mohammad <m.jafari9877@gmail.com>
2019-06-03 02:42:40 +0430

    Search for similarity among carpets & Search for carpets which can be bought with the given budget implementation completed

--- carpet_funcs.py
+++ carpet_funcs.py
@@ -1,4 +1,6 @@
+import os
 from PIL import Image
+import cv2
 import numpy as np
 
 
@@ -9,10 +11,7 @@ def img_to_arr(path):
 	'''
 
 	img = Image.open(path)
-
-	width, height = img.size
-	if width != 300 or height != 400:
-		img = img.resize((300,400))
+	img = img.resize((300,400))
 
 	return np.array(img, 'uint8')
 
@@ -47,3 +46,97 @@ def img_mult(matrixa, matrixb):
 			ans[i][j] = (r%256,g%256,b%256)
 
 	return np.array(ans, 'uint8')
+
+
+def search_carpet(carpet_path, inventory_dir):
+	'''
+	Search for similarity among carpets
+	Returns a list containing three best matches
+	'''
+
+	# carpet = img_to_arr(carpet_path)
+	carpet = cv2.imread(carpet_path)
+	carpet_hist = cv2.calcHist([carpet],[0],None,[256],[0,256])
+	# Transform into 1d array
+	# carpet = carpet.flatten()
+
+	directory = inventory_dir
+	listdir = os.listdir(directory)
+
+	error_list = list()
+
+	for file in listdir:
+		filename = file
+		path = os.path.join(directory, filename)
+		# this_img = img_to_arr(path).flatten()
+		this_img = cv2.imread(path)
+		this_img_hist = cv2.calcHist([this_img],[0],None,[256],[0,256])
+		# l = min(len(carpet), len(this_img))
+		# error_percentage = np.mean(carpet[:l] != this_img[:l])
+		error = cv2.compareHist(carpet_hist, this_img_hist, cv2.HISTCMP_CORREL)
+
+		error_list.append([filename, error])
+
+	error_list = np.array(error_list, 'object')
+	sorted_percent_col = error_list[:, 1].argsort()[-1:-4:-1]
+
+	best_matches = list()
+	for i in sorted_percent_col:
+		best_matches.append(os.path.join('/static/outputs', error_list[i][0]))
+
+	return best_matches
+
+
+def shop_seeker(budget):
+	'''
+	Search for carpets which can be bought with the budget
+	Returns a list of carpets & maxmimum number of carpets can be bought
+	'''
+
+	price_list = list()
+	with open('prices.csv', 'r') as f:
+		lines = list(map(str.strip, f.readlines()))
+		for line in lines:
+			# print('line')
+			# print('============================================')
+			# print(line)
+			price_list.append([
+				line.split(',')[0],
+				int(line.split(',')[1])
+			])
+		print(price_list)
+
+	price_list = np.array(price_list, 'object')
+	sorted_index = price_list[:, 1].argsort()
+
+	sum_temp = 0
+	carpet_counter = 0
+
+	maxmimum_number = 0
+
+	carpet_list = list()
+	prices = list()
+	for i in sorted_index:
+		print('--------sasasasasas0000000000000---------')
+		print(price_list[i])
+
+		if price_list[i][1] > budget:
+			continue
+
+		if (sum_temp + int(price_list[i][1])) < budget:
+			maxmimum_number += 1
+
+
+		sum_temp += price_list[i][1]
+
+		carpet_list.append((
+			os.path.join('/static/outputs', price_list[i][0]),
+			int(price_list[i][1])
+		))
+
+		# prices.append(
+		# 	int(price_list[i][1])
+		# )
+
+	# return carpet_list, prices, maxmimum_number
+	return carpet_list, maxmimum_number
--- web.py
+++ web.py
@@ -1,23 +1,25 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template, send_file
 from werkzeug.utils import secure_filename
-from carpet_funcs import arr_to_img, img_to_arr, img_mult
+from carpet_funcs import arr_to_img, img_to_arr, img_mult, search_carpet, shop_seeker
 
 
 UPLOAD_FOLDER = '/home/mohammad/Desktop/stuff/stuff3/carpet/carpet/static/uploads'
 STATIC_FOLDER = '/home/mohammad/Desktop/stuff/stuff3/carpet/carpet/static'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
+
 app = Flask(__name__, static_url_path='/static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['STATIC_URL_PATH'] = STATIC_FOLDER
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/design', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
 
@@ -34,40 +36,105 @@ def upload_file():
         # If user does not select file, browser also
         # submits an empty part without filename
         if file.filename == '' or \
-           filter_img.filename == '':
+           filter_img.filename == '' or\
+           request.form['price'] == '':
 
-            flash('No selected file')
+            flash('Fill all fields, please')
             return redirect(request.url)
+
+        price = int(request.form['price'])
 
         # If two files were given and they're allowed save them
         if file and \
            filter_img and \
+           price and \
            allowed_file(file.filename) and \
            allowed_file(filter_img.filename):
 
             filename = secure_filename(file.filename)
-            filter_img_name = secure_filename(filter_img.filename)
-
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
 
-            filter_img_path = os.path.join(app.config['UPLOAD_FOLDER'], filter_img_name)
+            filter_img_name = secure_filename(filter_img.filename)
+            filter_img_path = os.path.join(
+                                app.config['UPLOAD_FOLDER'],
+                                filter_img_name
+                              )
+
             filter_img.save(filter_img_path)
 
-            image = img_to_arr(file_path)
-            filter_image = img_to_arr(filter_img_path)
+            image_array = img_to_arr(file_path)
+            filter_image_array = img_to_arr(filter_img_path)
 
-            multed_image = img_mult(image, filter_image)
+            multed_image = img_mult(image_array, filter_image_array)
+
             output_image = arr_to_img(multed_image)
+            output_image_path = os.path.join(
+                                    app.config['STATIC_URL_PATH'],
+                                    '{}/{}'.format('outputs', filename)
+                                )
 
-            output_image.save(os.path.join(app.config['STATIC_URL_PATH'], '{}/{}'.format('outputs', filename)))
+            output_image.save(output_image_path)
 
-            return send_file(os.path.join(
-                app.config['STATIC_URL_PATH'], '{}/{}'.format('outputs', filename)
-            ))
+            with open('prices.csv', 'a') as f:
+                f.write('{},{}\n'.format(filename, price))
+
+            return send_file(output_image_path)
 
 
-    return render_template('carpet_upload.html', name='name')
+    return render_template('carpet_upload.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search_by_img():
+    if request.method == 'POST':
+
+        # check if the post request has the file part
+        if 'img' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        img = request.files['img']
+
+        # If user does not select file, browser also
+        # submits an empty part without filename
+        if img.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        # If two files were given and they're allowed save them
+        if img and allowed_file(img.filename):
+            filename = secure_filename(img.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            img.save(file_path)
+            best_matches = search_carpet(
+                                file_path,
+                                '/home/mohammad/Desktop/stuff/stuff3/carpet/carpet/static/outputs'
+                            )
+
+        print(best_matches)
+        return render_template('best_matches.html', best_matches=best_matches)
+
+
+    return render_template('search.html')
+
+
+@app.route('/buy', methods=['GET', 'POST'])
+def buy():
+    if request.method == 'POST':
+        if request.form['price'] == '':
+            return redirect(request.url)
+
+        budget = int(request.form['price'])
+        # carpet_list, prices, max_number = shop_seeker(budget)
+        carpet_list, max_number = shop_seeker(budget)
+
+        print('\n\n\n\nHEREEEEEEEEEE')
+        print(carpet_list)
+        # return render_template('buy.html', carpet_list=carpet_list, prices=prices, max_number=max_number)
+        return render_template('buy.html', carpet_list=carpet_list, max_number=max_number)
+
+    return render_template('buy.html')
 
 
 if __name__ == '__main__':
